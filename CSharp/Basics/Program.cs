10ab7cba3088f0c7b1aa4175153246551ab91878
jokgarcia <33973877+jokgarcia@users.noreply.github.com>
2020-02-22 14:16:26 +0800

    Commented Main Method Logic

--- CSharp/Basics/Program.cs
+++ CSharp/Basics/Program.cs
@@ -12,33 +12,34 @@ namespace Basics
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Customer customer = new Customer();
-            Product product = new Product();
-            ProductInformation productInformation = new ProductInformation();
+            //Customer customer = new Customer();
+            //Product product = new Product();
+            //ProductInformation productInformation = new ProductInformation();
 
             //customer.FirstName = "Jok";
 
-            Console.WriteLine("Enter Name : ");
-            customer.FirstName = Console.ReadLine();
-            
-            Console.WriteLine("Hello! " + customer.FirstName);
+            //Console.WriteLine("Enter Name : ");
+            //customer.FirstName = Console.ReadLine();
 
-            Console.WriteLine("What is your order ? ");
-            product.ProductName = Console.ReadLine();
+            //Console.WriteLine("Hello! " + customer.FirstName);
 
-            product.Price = productInformation.GetPrice(product.ProductName);
-            
-            if(product.Price > 0) 
-            {
-                Console.WriteLine("The Price of " + 
-                                  product.ProductName 
-                                  + " is " + product.Price);
-            }
-            else
-            {
-                Console.WriteLine(product.ProductName + " is Not Found");
-            }
-            
+            //Console.WriteLine("What is your order ? ");
+            //product.ProductName = Console.ReadLine();
+
+            //product.Price = productInformation.GetPrice(product.ProductName);
+
+            //if (product.Price > 0)
+            //{
+            //    Console.WriteLine("The Price of " +
+            //                      product.ProductName
+            //                      + " is " + product.Price);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(product.ProductName + " is Not Found");
+            //}
+
+
         }
     }
 }
