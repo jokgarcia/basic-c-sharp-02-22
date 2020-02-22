64e3466bbc9381dcc47d0d2b3d7bce9cc6011325
jomarmontuya <jomar.montuya@outlook.com>
2020-02-22 14:07:03 +0800

    Added Supplier Class

---
+++ CSharp/Basics/Model/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics.Model
+{
+    class Supplier
+    {
+        public string Alibaba { get; set; }
+        public string Lazada { get; set; }
+        public string Amazon { get; set; }
+        public string Ebay { get; set; }
+    }
+}
